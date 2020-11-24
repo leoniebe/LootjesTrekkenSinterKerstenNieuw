c357690dea96b3e4bb811013687961d04251bfcc
Victor Langemaire <victor.langemaire@tutanota.com>
2020-11-24 19:56:07 +0100

    Send form

---
+++ LootjesTrekken/Form2.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace LootjesTrekken
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVersturen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVersturen
+            // 
+            this.btnVersturen.Location = new System.Drawing.Point(510, 299);
+            this.btnVersturen.Name = "btnVersturen";
+            this.btnVersturen.Size = new System.Drawing.Size(75, 23);
+            this.btnVersturen.TabIndex = 0;
+            this.btnVersturen.Text = "Versturen";
+            this.btnVersturen.UseVisualStyleBackColor = true;
+            this.btnVersturen.Click += new System.EventHandler(this.btnVersturen_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVersturen);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVersturen;
+    }
+}
+
---
+++ LootjesTrekken/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LootjesTrekken
+{
+    public partial class Form2 : Form
+    {
+        List<string> emailAdresses;
+
+        public Form2()
+        {
+            InitializeComponent();
+            emailAdresses.Add("victor.langemaire@tutanota.com");
+            emailAdresses.Add("leoniebekkernens@gmail.com");
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnVersturen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
